aa5d42a474610033808177bbe82f442bfb5b6818
WK <mwk0725@gmail.com>
2023-07-28 11:12:42 +0900

    feat: Theme 모드 상태 context로 구현 #11

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       components: resolve(__dirname, './src/components'),
       pages: resolve(__dirname, './src/pages'),
       styles: resolve(__dirname, './src/styles'),
+      contexts: resolve(__dirname, './src/contexts'),
     },
   },
 });
