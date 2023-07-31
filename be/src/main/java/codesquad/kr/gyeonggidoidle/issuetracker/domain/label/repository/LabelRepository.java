ec6e8bcffb5ea6d3a6ca750d15e5962afb4944c6
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-31 16:41:51 +0900

    test: LabelRepositoryTest 작성 (#15)

---
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/label/repository/LabelRepository.java
@@ -0,0 +1,40 @@
+package codesquad.kr.gyeonggidoidle.issuetracker.domain.label.repository;
+
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.label.repository.VO.LabelVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class LabelRepository {
+
+    private final NamedParameterJdbcTemplate template;
+
+    @Autowired
+    public LabelRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<LabelVO> findAllByIssueId(Long issueId) {
+        String sql = "SELECT l.name, l.background_color, l.text_color " +
+                "FROM label AS l " +
+                "LEFT JOIN issue_label AS i " +
+                "ON l.id = i.label_id " +
+                "WHERE i.issue_id = :issueId";
+
+        return template.query(sql, Map.of("issueId", issueId), labelRowMapper());
+    }
+
+    private final RowMapper<LabelVO> labelRowMapper() {
+        return ((rs, rowNum) -> LabelVO.builder()
+                .name(rs.getString("name"))
+                .backgroundColor(rs.getString("background_color"))
+                .textColor(rs.getString("text_color"))
+                .build());
+    }
+}
