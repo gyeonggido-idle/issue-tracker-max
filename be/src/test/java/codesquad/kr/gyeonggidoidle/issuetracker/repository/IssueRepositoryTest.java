e532168c95b5c488714143e08afc849cef7bb95a
n-aa-g <wlsdnjs0124@gmail.com>
2023-07-31 15:01:35 +0900

    test: IssueRepositoryTest 작성 (#15)
    이슈 레포지토리 테스트 코드를 작성했습니다.

---
+++ be/src/test/java/codesquad/kr/gyeonggidoidle/issuetracker/repository/IssueRepositoryTest.java
@@ -0,0 +1,45 @@
+package codesquad.kr.gyeonggidoidle.issuetracker.repository;
+
+import codesquad.kr.gyeonggidoidle.issuetracker.annotation.RepositoryTest;
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.IssueRepository;
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.vo.IssueVO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RepositoryTest
+public class IssueRepositoryTest {
+
+    private final IssueRepository repository;
+
+    @Autowired
+    public IssueRepositoryTest(DataSource dataSource) {
+        this.repository = new IssueRepository(dataSource);
+    }
+
+    @DisplayName("DB에서 열린 이슈를 불러온다.")
+    @Test
+    void openIssuetest() {
+        List<IssueVO> actualValue = repository.findOpenIssues();
+
+        assertThat(actualValue.size()).isEqualTo(2);
+        assertThat(actualValue.get(1).getTitle()).isEqualTo("제목 3");
+        assertThat(actualValue.get(0).getAuthor()).isEqualTo("nag");
+    }
+
+    @DisplayName("DB에서 닫힌 이슈를 불러온다.")
+    @Test
+    void closedIssuetest() {
+        List<IssueVO> actualValue = repository.findClosedIssues();
+
+        assertThat(actualValue.size()).isEqualTo(3);
+        assertThat(actualValue.get(0).getId()).isEqualTo(2);
+        assertThat(actualValue.get(2).getMilestone()).isEqualTo("마일스톤 1");
+
+    }
+}
