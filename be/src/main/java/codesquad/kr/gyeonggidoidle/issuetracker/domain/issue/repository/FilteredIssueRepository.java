c882a75dcffec21fbc1be715bba1ee4c36da7fd1
n-aa-g <wlsdnjs0124@gmail.com>
2023-08-09 19:50:14 +0900

    feat: FilteredIssueRepository 생성
    쿼리를 제외하고 FilteredIssueRepository를 만들었습니다.

--- be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/issue/repository/FilteredIssueRepository.java
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/issue/repository/FilteredIssueRepository.java
@@ -1,8 +1,13 @@
 package codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository;
 
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.FilterCondition;
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.vo.IssueVO;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -10,4 +15,12 @@ public class FilteredIssueRepository {
 
     private final SqlSessionFactory sqlSessionFactory;
 
+    public List<IssueVO> findByFilter(FilterCondition filterCondition) {
+        List<IssueVO> filteredIssues = null;
+
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            filteredIssues = sqlSession.selectList(filter.findByfilter, filterCondition);
+        }
+        return filteredIssues;
+    }
 }
