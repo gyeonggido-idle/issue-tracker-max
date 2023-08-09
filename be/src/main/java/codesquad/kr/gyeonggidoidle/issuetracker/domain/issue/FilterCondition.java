298176903fce865c7b953ab6d3755934c88be971
n-aa-g <wlsdnjs0124@gmail.com>
2023-08-09 19:50:14 +0900

    feat: FilteredIssueRepository 생성
    쿼리를 제외하고 FilteredIssueRepository를 만들었습니다.

---
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/issue/FilterCondition.java
@@ -0,0 +1,23 @@
+package codesquad.kr.gyeonggidoidle.issuetracker.domain.issue;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class FilterCondition {
+
+    private final Boolean isOpen;
+    private final String assignee;
+    private final String label;
+    private final String milestone;
+    private final String author;
+
+    @Builder
+    private FilterCondition(Boolean isOpen, String assignee, String label, String milestone, String author) {
+        this.isOpen = isOpen;
+        this.assignee = assignee;
+        this.label = label;
+        this.milestone = milestone;
+        this.author = author;
+    }
+}
