38196fd4c22579689daa369ba9c1b59ee33f7d3e
n-aa-g <wlsdnjs0124@gmail.com>
2023-08-14 21:36:42 +0900

    refactor: IssueController에서 FilterController 분리
    IssueController가 이슈와 필터를 관리하는 책임을 가지고 있어 필터만을 관리하는 FilterController를 생성했습니다.

---
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/filter/controller/FilterController.java
@@ -0,0 +1,26 @@
+package codesquad.kr.gyeonggidoidle.issuetracker.domain.filter.controller;
+
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.controller.response.FilterListResponse;
+import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.service.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/filters")
+public class FilterController {
+
+    private final IssueService issueService;
+
+    @GetMapping("/main")
+    public FilterListResponse readFilters() {
+        return FilterListResponse.from(issueService.readFilters());
+    }
+
+    @GetMapping("/detail")
+    public FilterListResponse readFiltersFromIssue() {
+        return FilterListResponse.from(issueService.readFiltersFromIssue());
+    }
+}
