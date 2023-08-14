1be4ced246f8e97e05a08bc2ad85b986baabb244
n-aa-g <wlsdnjs0124@gmail.com>
2023-08-15 00:04:39 +0900

    feat: 쓰지 않는 메서드 정리
    IssueRepository의 readOpenIssues와 readClosedIssues 메서드를 삭제했습니다.

--- be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/issue/repository/IssueRepository.java
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/domain/issue/repository/IssueRepository.java
@@ -4,7 +4,6 @@ import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.Issue;
 import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.vo.IssueStatusVO;
 import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.vo.IssueUpdateVO;
 import codesquad.kr.gyeonggidoidle.issuetracker.domain.issue.repository.vo.IssueVO;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -13,7 +12,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -29,36 +27,6 @@ public class IssueRepository {
             .title(rs.getString("title"))
             .createdAt(rs.getTimestamp("created_at").toLocalDateTime())
             .build();
-
-    public List<IssueVO> findOpenIssues() {
-        String sql = "SELECT issue.id, " +
-                "milestone.name AS milestone_name, " +
-                "member.name AS author_name, " +
-                "issue.title, " +
-                "issue.created_at " +
-                "FROM issue " +
-                "LEFT JOIN member ON issue.author_id = member.id " +
-                "LEFT JOIN milestone ON issue.milestone_id = milestone.id " +
-                "WHERE issue.is_open = true AND issue.is_deleted = false " +
-                "ORDER BY issue.id DESC";
-
-        return template.query(sql, issueVOMapper);
-    }
-
-    public List<IssueVO> findClosedIssues() {
-        String sql = "SELECT issue.id, " +
-                "milestone.name AS milestone_name, " +
-                "member.name AS author_name, " +
-                "issue.title, " +
-                "issue.created_at " +
-                "FROM issue " +
-                "LEFT JOIN member ON issue.author_id = member.id " +
-                "LEFT JOIN milestone ON issue.milestone_id = milestone.id " +
-                "WHERE issue.is_open = false AND issue.is_deleted = false " +
-                "ORDER BY issue.id DESC";
-
-        return template.query(sql, issueVOMapper);
-    }
 
     public void updateIssuesStatus(IssueStatusVO vo) {
         String sql = "UPDATE issue SET is_open = :is_open WHERE id IN (:issueIds)";
